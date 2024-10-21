fc85519637e125d7d8ad6eededffa0903433bca5
Alexey Ustinnikov <a.ustinnikov@grfc.ru>
2024-10-21 22:23:38 +0300

    init

---
+++ bot.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from telebot.async_telebot import AsyncTeleBot
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+bot = AsyncTeleBot(config["BOT_API_KEY"])
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=["help", "start"])
+async def send_welcome(message):
+    text = "Hi, I am EchoBot.\nJust write me something and I will repeat it!"
+    await bot.reply_to(message, text)
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+
+asyncio.run(bot.polling())
